453f4144f60220c9e517e2925f78869a7df777df
Harshil Patel <71842610+Harshil-P21@users.noreply.github.com>
2023-05-11 14:56:36 -0400

    Update nbaTestWebScrape.py

--- nbaTestWebScrape.py
+++ nbaTestWebScrape.py
@@ -17,12 +17,12 @@ soup.findAll('tr', limit=2)
 # use getText()to extract the text we need into a list
 headers = [th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('th')]
 headers.remove('Date')
-# print (headers)
+
 # exclude the first column as we will not need the ranking order from Basketball Reference for the analysis
 rows = soup.findAll('tr')[1:6]
 player_stats = [[td.getText() for td in rows[i].findAll('td')]
             for i in range(len(rows))]
-# print (player_stats)
+
 stats = pd.DataFrame(player_stats, columns = headers)
 stats.head(10)
-print (stats)
+

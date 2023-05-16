b6a76a35596070652559716b74654cbf3ee480eb
Harshil Patel <71842610+Harshil-P21@users.noreply.github.com>
2023-05-16 17:13:21 -0400

    Updated code to handle cases where more than one NBA player has the same name

--- nbaTestWebScrape.py
+++ nbaTestWebScrape.py
@@ -3,16 +3,19 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import sys
 
+count = 1
+playerPage = '0'+str(count)
 playerInfo = str(input("Please enter players first and last name as follows'LeBron James': "))
 while playerInfo != 'n':
     playerName = playerInfo
     playerInfo = playerInfo.replace(".","")
-    playerInfo = (playerInfo.lower()).split()
+    playerInfo = playerInfo.replace("'","")
+    playerInfoList = (playerInfo.lower()).split()
 
     if (len(playerInfo[1]) >= 5):
-        url = f"https://www.basketball-reference.com/players/{playerInfo[1][0]}/{playerInfo[1][:5]+playerInfo[0][:2]+'01'}.html"
+        url = f"https://www.basketball-reference.com/players/{playerInfoList[1][0]}/{playerInfoList[1][:5]+playerInfoList[0][:2]+playerPage}.html"
     else:
-        url = f"https://www.basketball-reference.com/players/{playerInfo[1][0]}/{playerInfo[1][:]+playerInfo[0][:2]+'01'}.html"
+        url = f"https://www.basketball-reference.com/players/{playerInfoList[1][0]}/{playerInfoList[1][:]+playerInfoList[0][:2]+playerPage}.html"
     html = urlopen(url)
     soup = BeautifulSoup(html,'html.parser')
 
@@ -39,16 +42,21 @@ while playerInfo != 'n':
 
     print (stats)
 
-
-    rawStatsWanted = (str(input("What stat would you like to see the average of for the past 5 games the player has played (enter 'q' to stop or 'n' for new player): ")))
-    while (rawStatsWanted != 'q' and rawStatsWanted != 'n'):
-        statsWanted = (rawStatsWanted).lower()
-        for col in stats.columns:
-            if ((str(col)).lower() == statsWanted):
-                stats = stats.astype({col:'float'})
-                print (f"The average {rawStatsWanted} for {playerName} is {round(stats[col].mean(),1)}")
+    nextPlayer = str(input("If this isn't the correct player but has the same name type 'y', if it is the correct player enter 'n': "))
+    while (nextPlayer != 'y'):
         rawStatsWanted = (str(input("What stat would you like to see the average of for the past 5 games the player has played (enter 'q' to stop or 'n' for new player): ")))
-    if (rawStatsWanted == 'q'):
-        sys.exit()
-    else:
-        playerInfo = str(input("Please enter players first and last name as follows'LeBron James': "))
+        while (rawStatsWanted != 'q' and rawStatsWanted != 'n'):
+            statsWanted = (rawStatsWanted).lower()
+            for col in stats.columns:
+                if ((str(col)).lower() == statsWanted):
+                    stats = stats.astype({col:'float'})
+                    print (f"The average {rawStatsWanted} for {playerName} is {round(stats[col].mean(),1)}")
+            rawStatsWanted = (str(input("What stat would you like to see the average of for the past 5 games the player has played (enter 'q' to stop or 'n' for new player): ")))
+        if (rawStatsWanted == 'q'):
+            sys.exit()
+        else:
+            playerInfo = str(input("Please enter players first and last name as follows'LeBron James': "))
+    if (nextPlayer =='y'):
+        count+= 1
+        playerPage = '0'+str(count)
+    
